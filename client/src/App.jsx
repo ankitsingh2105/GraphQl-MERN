378c64bfe616ef243faaf5b5ee66b2a49609dc3d
Ankit <ankitchauhan21500@gmail.com>
2024-06-26 17:33:47 +0530

    showing the data on the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,58 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { useQuery, ApolloClient } from '@apollo/client';
+import { gql, useLazyQuery } from '@apollo/client';
 
 
-const query = `
-  query op{
-  getUsers {
-    name
-    email
-    website
+const query = gql`
+query listGames{
+  games {
+    title
+    platform
   }
 }
 `
 
 function App() {
   const [count, setCount] = useState(0)
-  const { data, loading } = useQuery(query)
-  useEffect(()=>{
-    console.log("this is the data :: " , data);
-  },[data])
+  const { data : gameData, loading, error } = useQuery(query)
+  useEffect(() => {
+    console.log("this is the gameData :: ", gameData);
+  },[gameData])
   return (
     <>
+      {
+        !loading ?
+          <>
+            {
+              gameData.games.map((e, index) => {
+                return (
+                  <>
+                    <section key={index} >
+                      <div key={index} >{e.id}</div>
+                      <h2 >{e.title}</h2>
+                      <div>
+                        {
+                          e.platform.map((ee, index) => {
+                            return (
+                              <div key={ee}>{ee}</div>
+                            )
+                          })
+                        }
+                      </div>
+                      <br />
+                    </section>
+                  </>
+
+                )
+              })
+            }
+          </>
+          :
+          <>
+            <div>loading........</div>
+          </>
+      }
     </>
   )
 }
